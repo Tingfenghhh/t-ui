e955650f1c7d4028f20f8728351d0b19eb7d34ca
zxd <shisanlailin@outlook.com>
2023-10-26 15:04:12 +0800

    feat: 更改包名

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import TingfengUI from '../tingfeng-ui-test/ting-feng-ui.js'
-import '../tingfeng-ui-test/style.css'
+import TingfengUI from '../tingfeng-ui/tf-ui'
+import '../tingfeng-ui/style.css'
 
 const app = createApp(App)
 app.use(TingfengUI)
--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
   plugins: [vue(), dts(
   )],
   build: {
-    outDir: "tingfeng-ui-test", //输出文件名称
+    outDir: "tingfeng-ui", //输出文件名称
     lib: {
       entry: "./packages/index.ts", //指定组件编译入口文件
       name: "ting-feng-ui", //指定组件库的名称
-      fileName: "ting-feng-ui",
+      fileName: "tf-ui",
     }, //库编译模式配置
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
